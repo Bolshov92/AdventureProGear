7c467a6db2b31a7a1ca51678165fcb377091c085
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-12-02 21:51:43 +0200

    removed unused imports

--- src/main/java/com/example/adventureprogearjava/repositories/ProductRepository.java
+++ src/main/java/com/example/adventureprogearjava/repositories/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.example.adventureprogearjava.repositories;
 
 import com.example.adventureprogearjava.entity.Product;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
